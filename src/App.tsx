2610b993ee0e5c243bc4e6fe11ac1ec2a36d450b
joshua <joshua@naver.com>
2024-08-10 11:03:03 +0900

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DrawerLayout from "./components/layout/DrawerLayout";
+import Home from "./components/layout/Home";
+
 function App() {
-  return <div className="text-sky-300">hello 팀원들</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DrawerLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
